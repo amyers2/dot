f8fca0ecaf77163f79cb8606d250ea2303ea9ac6
amyers2 <arm3112@gmail.com>
2024-05-08 11:40:07 -0400

    Updates to telescope options; Added treesitter plugin and options.

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,40 @@
 local builtin = require('telescope.builtin')
 
+require('telescope').setup{
+   defaults = {
+      -- configure to use ripgrep
+      vimgrep_arguments = {
+         "rg",
+         "--follow",       -- follow symbolic links
+         "--hidden",       -- search for hidden files
+         "--line-number",  -- show line numbers
+         "--smart-case",   -- smart case search
+
+         -- Exclude some patterns from search
+         "--glob=!**/.git/*",
+         "--glob=!**/.idea/*",
+         "--glob=!**/.vscode/*",
+         "--glob=!**/build/*",
+      },
+   },
+   pickers = {
+      find_files = {
+         hidden = true,
+         -- needed to exclude some files & dirs from general search
+         -- when not included or specified in .gitignore
+         find_command = {
+            "rg",
+            "--files",
+            "--hidden",
+            "--glob=!**/.git/*",
+            "--glob=!**/.idea/*",
+            "--glob=!**/.vscode/*",
+            "--glob=!**/build/*",
+         },
+      },
+   }, 
+}
+
 -- This allows us to have a "git files search", and an "all files search".
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
