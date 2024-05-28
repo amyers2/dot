5f7a0da558b8f1b4cfba98df045b59fd7f00d733
Anthony Myers <arm3112@gmail.com>
2024-05-27 21:46:27 -0400

    modified remap script with a comment

--- scripts/remap-capslock-to-ctrl.sh
+++ scripts/remap-capslock-to-ctrl.sh
@@ -1,5 +1,5 @@
+setxkbmap -option ctrl:nocaps # this makes capslock into ctrl
 #setxkbmap -option caps:ctrl
-setxkbmap -option ctrl:nocaps
 #setxkbmap -option caps:escape
 #setxkbmap -option ctrl:ralt_rctrl
 #setxkbmap -option ctrl:swap_lalt_lctl
