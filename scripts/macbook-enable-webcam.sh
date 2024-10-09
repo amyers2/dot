226c542713e1edc8695d0e71d6cd0ee7fb05f41b
Anthony Myers <arm3112@gmail.com>
2024-10-09 18:33:44 -0400

    Added script to enable webcam on macbook. Updates to tmux conf.

---
+++ scripts/macbook-enable-webcam.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#TODO: navigate to the directory of this script, first
+
+# Navigate to facetimehd-firmware dl location
+cd ../../git/facetimehd-firmware || exit
+make
+sudo make install
+
+# Navigate to bcwc_pcie dl location
+cd ../bcwc_pcie || exit
+make
+sudo make install
+sudo depmod
+sudo modprobe facetimehd
+
+# Test it out
+mplayer tv://
