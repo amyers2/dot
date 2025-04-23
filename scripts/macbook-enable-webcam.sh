4e114d52406ce3e8f7955d1e4898ef85fbe9bb03
Anthony Myers <arm3112@gmail.com>
2025-04-22 20:57:16 -0400

    Added a comment.

--- scripts/macbook-enable-webcam.sh
+++ scripts/macbook-enable-webcam.sh
@@ -2,6 +2,16 @@
 
 #TODO: navigate to the directory of this script, first
 
+#
+# NOTES:
+#
+# Around Apr 2025, this script was failing to enable
+# the webcam. After looking around everywhere, I found
+# a suggestion to clean the projects being built below.
+# After doing that, it finally worked. So I added the
+# `make clean` steps to each project.
+#
+
 # Navigate to facetimehd-firmware dl location
 cd ../../git/facetimehd-firmware || exit
 make clean
